d739e1f626d4bffb952adb123e7cfb42130bfa31
sungyeon <24203738+YeoniH@users.noreply.github.com>
2024-10-27 11:53:06 +1100

    New script created - under construction

---
+++ stability.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy.optimize import fsolve
+from numpy.linalg import eig
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from scipy.linalg import solve_continuous_lyapunov
+
+
